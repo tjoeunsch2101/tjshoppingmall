2ad6536e7c45f3ee78a1b0afe572c9acb007bb52
lew <bit4math@gmail.com>
2024-05-03 13:48:09 +0900

    else if(jqXHR.status == '403'){                         alert('주문할 상품을 체크해 주세요');                         location.href='/cart';  }

--- src/main/java/com/tjoeun/shop/config/SecurityConfig.java
+++ src/main/java/com/tjoeun/shop/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin()
                 .loginPage("/member/login")
-                .defaultSuccessUrl("/",  true)
+                .defaultSuccessUrl("/", true)
                 .usernameParameter("email")
                 .failureUrl("/member/login/error")
                 .and()
